a919bc529cc8e773e6728e68b84f9eaea8dfd30b
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2026-04-22 18:57:16 +0530

    feat: add Topbar component to display patient metadata, coding metrics, and navigation actions

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToastService } from '../../services/toast.service';
+import { CodingDataService } from '../../services/coding-data.service';
 
 @Component({
   selector: 'app-topbar',
@@ -7,8 +8,23 @@ import { ToastService } from '../../services/toast.service';
   styleUrls: ['./topbar.component.scss'],
   standalone: false
 })
-export class TopbarComponent {
-  constructor(private toastService: ToastService) {}
+export class TopbarComponent implements OnInit {
+  icdCount = 0;
+  icdGapCount = 0;
+  cptCount = 0;
+  cptGapCount = 0;
+
+  constructor(
+    private toastService: ToastService,
+    private codingService: CodingDataService
+  ) {}
+
+  ngOnInit() {
+    this.codingService.hccData$.subscribe(data => this.icdCount = data.length);
+    this.codingService.gapData$.subscribe(data => this.icdGapCount = data.length);
+    this.codingService.cptData$.subscribe(data => this.cptCount = data.length);
+    this.codingService.cptGapData$.subscribe(data => this.cptGapCount = data.length);
+  }
 
   showToast(msg: string) {
     this.toastService.showToast(msg);
